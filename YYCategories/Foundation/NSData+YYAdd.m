c2bae28403a355ff19c5f8be698d84482ee685b7
fanpyi <fanpyi@163.com>
2015-12-03 12:51:35 +0800

    修改了md5和gitignore

--- YYCategories/Foundation/NSData+YYAdd.m
+++ YYCategories/Foundation/NSData+YYAdd.m
@@ -58,13 +58,11 @@ YYSYNTH_DUMMY_CLASS(NSData_YYAdd)
 - (NSString *)md5String {
     unsigned char result[CC_MD5_DIGEST_LENGTH];
     CC_MD5(self.bytes, (CC_LONG)self.length, result);
-    return [NSString stringWithFormat:
-            @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
-            result[0], result[1], result[2], result[3],
-            result[4], result[5], result[6], result[7],
-            result[8], result[9], result[10], result[11],
-            result[12], result[13], result[14], result[15]
-            ];
+    NSMutableString *r = [NSMutableString string];
+    for (int j = 0 ; j < CC_MD5_DIGEST_LENGTH; j++) {
+        [r appendFormat:@"%02x",result[j]];
+    }
+    return r;
 }
 
 - (NSData *)md5Data {
